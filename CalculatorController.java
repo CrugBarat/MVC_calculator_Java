603eb7e7d87a66d2a1e7b116970547b0f21f48ed
CrugBarat <craigtbarratt@gmail.com>
2020-05-26 16:33:05 +0100

    adds calc listener inner class
    Signed-off-by: CrugBarat <craigtbarratt@gmail.com>

--- CalculatorController.java
+++ CalculatorController.java
@@ -11,6 +11,35 @@ public class CalculatorController {
 	public CalculatorController(CalculatorView theView, CalculatorModel theModel) {
 		this.theView = theView;
 		this.theModel = theModel;
+
+		//calc listener
+		this.theView.addCalculateListener(new CalculateListener());
+	}
+
+  //add calc listener inner class
+	class CalculateListener implements ActionListener{
+
+		public void actionPerformed(ActionEvent e) {
+
+			int firstNumber, secondNumber = 0;
+
+	    //add try block for improper number entry
+			try{
+
+				firstNumber = theView.getFirstNumber();
+				secondNumber = theView.getSecondNumber();
+
+				theModel.add(firstNumber, secondNumber);
+				theView.setCalcSolution(theModel.getResult());
+			}
+
+			catch(NumberFormatException ex){
+				System.out.println(ex);
+				theView.displayErrorMessage("You Need to Enter 2 Numbers");
+			}
+
+		}
+
 	}
 
 }
